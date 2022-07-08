53ed87b84d0a5e3a9069c6cb63511ee8411297a3
daudi13 <davouma13@gmail.com>
2022-07-08 17:37:01 +0300

    add test for POST/books

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -1,14 +1,28 @@
 require 'rails_helper'
 
 describe'Books API', type: :request do
-  it 'returns all books' do
-    FactoryBot.create(:book, title: 'Book 1', author: 'Author 1', genre: 'Genre 1', current_page: 1, total_pages: 100)
-    FactoryBot.create(:book, title: 'Book 2', author: 'Author 2', genre: 'Genre 2', current_page: 2, total_pages: 100)
-    FactoryBot.create(:book, title: 'Book 3', author: 'Author 3', genre: 'Genre 3', current_page: 3, total_pages: 100)
+  
+  describe 'GET/books' do
+    it 'returns all books' do
+      FactoryBot.create(:book, title: 'Book 1', author: 'Author 1', genre: 'Genre 1', current_page: 1, total_pages: 100)
+      FactoryBot.create(:book, title: 'Book 2', author: 'Author 2', genre: 'Genre 2', current_page: 2, total_pages: 100)
+      FactoryBot.create(:book, title: 'Book 3', author: 'Author 3', genre: 'Genre 3', current_page: 3, total_pages: 100)
+  
+      get '/api/v1/books'
+  
+      expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body).size).to eq(3)
+    end
+  end
 
-    get '/api/v1/books'
-
-    expect(response).to have_http_status(:success)
-    expect(JSON.parse(response.body).size).to eq(3)
+  describe 'POST/books' do
+    it 'creates a new book' do
+      expect {
+        post '/api/v1/books', params: { book: { title: 'Book 1', author: 'Author 1', genre: 'Genre 1', current_page: 1, total_pages: 100 } }
+      }.to change { Book.count }.by(1)
+  
+      expect(response).to have_http_status(:created)
+      expect(JSON.parse(response.body)['title']).to eq('Book 1')
+    end
   end
 end
