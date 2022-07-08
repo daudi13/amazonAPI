3b088e88223d9b36416196e1b253c25aaa93c5a7
daudi13 <davouma13@gmail.com>
2022-07-08 19:14:01 +0300

    Add tests

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -3,11 +3,13 @@ require 'rails_helper'
 describe'Books API', type: :request do
   
   describe 'GET/books' do
-    it 'returns all books' do
+    before do 
       FactoryBot.create(:book, title: 'Book 1', author: 'Author 1', genre: 'Genre 1', current_page: 1, total_pages: 100)
       FactoryBot.create(:book, title: 'Book 2', author: 'Author 2', genre: 'Genre 2', current_page: 2, total_pages: 100)
       FactoryBot.create(:book, title: 'Book 3', author: 'Author 3', genre: 'Genre 3', current_page: 3, total_pages: 100)
-  
+    end
+
+    it 'returns all books' do
       get '/api/v1/books'
   
       expect(response).to have_http_status(:success)
@@ -27,11 +29,14 @@ describe'Books API', type: :request do
   end
 
   describe 'DELETE /books/:id' do
+    let!(:book) { FactoryBot.create(:book, title: 'Book 1', author: 'Author 1', genre: 'Genre 1', current_page: 1, total_pages: 100) }
+    
     it 'deletes a specific book' do
-      FactoryBot.create(:book, title: 'Book 1', author: 'Author 1', genre: 'Genre 1', current_page: 1, total_pages: 100)
-      delete '/api/v1/books/1' do
-        expect(response).to have_http_status(:no_content)
-      end
+      expect {
+        delete "/api/v1/books/#{book.id}"
+      }.to change { Book.count }.from(1).to(0)
+
+      expect(response).to have_http_status(:no_content)
     end
   end
 end
