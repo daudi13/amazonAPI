cfd3eb61fdb00a036dd5e2f4a00e0e1434fbba9f
daudi13 <davouma13@gmail.com>
2022-07-08 13:45:35 +0300

    Add validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,7 @@
 class Book < ApplicationRecord
+  validates :author, presence: true, length: { minimum: 2 }
+  validates :title, presence: true, length: { minimum: 2 }
+  validates :genre, presence: true, length: { minimum: 2 }
+  validates :current_page, presence: true, numericality: { only_integer: true }
+  validates :total_pages, numericality: { only_integer: true }
 end
