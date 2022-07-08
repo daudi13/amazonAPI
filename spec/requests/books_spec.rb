1e0a753a93682b00d8c91f766965744214f6cf6f
daudi13 <davouma13@gmail.com>
2022-07-08 17:55:08 +0300

    Add test for DELETE

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -25,4 +25,13 @@ describe'Books API', type: :request do
       expect(JSON.parse(response.body)['title']).to eq('Book 1')
     end
   end
+
+  describe 'DELETE /books/:id' do
+    it 'deletes a specific book' do
+      FactoryBot.create(:book, title: 'Book 1', author: 'Author 1', genre: 'Genre 1', current_page: 1, total_pages: 100)
+      delete '/api/v1/books/1' do
+        expect(response).to have_http_status(:no_content)
+      end
+    end
+  end
 end
