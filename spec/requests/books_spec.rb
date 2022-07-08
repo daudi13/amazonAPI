19fa2f01015556ae9006ac73dc5c9417305e97a7
daudi13 <davouma13@gmail.com>
2022-07-08 17:16:37 +0300

    add test for response.body count

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -2,9 +2,13 @@ require 'rails_helper'
 
 describe'Books API', type: :request do
   it 'returns all books' do
+    FactoryBot.create(:book, title: 'Book 1', author: 'Author 1', genre: 'Genre 1', current_page: 1, total_pages: 100)
+    FactoryBot.create(:book, title: 'Book 2', author: 'Author 2', genre: 'Genre 2', current_page: 2, total_pages: 100)
+    FactoryBot.create(:book, title: 'Book 3', author: 'Author 3', genre: 'Genre 3', current_page: 3, total_pages: 100)
+
     get '/api/v1/books'
 
     expect(response).to have_http_status(:success)
-    expect(JSON.parse(response.body).size).to eq(2)
+    expect(JSON.parse(response.body).size).to eq(3)
   end
 end
