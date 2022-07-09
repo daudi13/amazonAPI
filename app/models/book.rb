4e7709061e4bb0ed2ad0d40e8f8cf5c47adb8428
daudi13 <davouma13@gmail.com>
2022-07-09 08:49:44 +0300

    Add belong to association to book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,8 @@
 class Book < ApplicationRecord
-  validates :author, presence: true, length: { minimum: 2 }
   validates :title, presence: true, length: { minimum: 2 }
   validates :genre, presence: true, length: { minimum: 2 }
   validates :current_page, presence: true, numericality: { only_integer: true }
   validates :total_pages, numericality: { only_integer: true }
+
+  belongs_to :author
 end
