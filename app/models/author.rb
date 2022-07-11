3bdf33b51e306658ab4e593a93ca90b357a71724
daudi13 <davouma13@gmail.com>
2022-07-11 07:55:26 +0300

    in this commit: - correct second_name to last_name - change publisher from maximum to minimum

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,9 +1,9 @@
 class Author < ApplicationRecord
   validates :first_name, presence: true, length: { minimum: 1 }
-  validates :second_name, presence: true, length: { minimum: 1 }
+  validates :last_name, presence: true, length: { minimum: 1 }
   validates :age, numericality: { only_integer: true }
   validates :bio , presence: true, length: { maximum: 500 }
-  validates :publisher , presence: true, length: { minimum: 10 }
+  validates :publisher , presence: true, length: { minimum: 1 }
 
   has_many :books
 end
