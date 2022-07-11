89d4d3f419fa98ca4403747864b00900f9ab5f36
daudi13 <davouma13@gmail.com>
2022-07-11 07:31:34 +0300

    Add validation for author model

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,9 @@
 class Author < ApplicationRecord
+  validates :first_name, presence: true, length: { minimum: 1 }
+  validates :second_name, presence: true, length: { minimum: 1 }
+  validates :age, numericality: { only_integer: true }
+  validates :bio , presence: true, length: { maximum: 500 }
+  validates :publisher , presence: true, length: { minimum: 10 }
+
   has_many :books
 end
