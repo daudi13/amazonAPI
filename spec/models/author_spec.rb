11501d33693f031643cd761ca5e4da9b88bdac09
daudi13 <davouma13@gmail.com>
2022-07-11 08:02:07 +0300

    Add author model test

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {Author.new(first_name: 'John', last_name: 'Doe', age: 44, bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', publisher: 'Publishing')}
+  before { subject.save }
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'shold have a first_name' do
+    expect(subject.first_name).to eq('John')
+  end
+
+  it 'shold have a last_name' do
+    expect(subject.last_name).to eq( 'Doe')
+  end
+
+  it 'should have an age' do
+    expect(subject.age).to eq(44)
+  end
+
+  it 'should have a bio' do
+    expect(subject.bio).to eq('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
+  end
 end
