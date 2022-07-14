146eb7bbdd3e8f62ae385ec06dfd8c7e8098970e
daudi13 <davouma13@gmail.com>
2022-07-14 12:08:42 +0300

    Add authentification route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :books, only: [:index, :create, :destroy, :update, :show]
+
+      post 'authenticate', to: 'authentication#create'
     end
   end
 end
