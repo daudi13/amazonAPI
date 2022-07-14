8f3a2d56d63abfee35d677c67124f901ef0bab04
daudi13 <davouma13@gmail.com>
2022-07-14 08:40:32 +0300

    remove factory author default

--- spec/factories/authors.rb
+++ spec/factories/authors.rb
@@ -1,9 +1,4 @@
 FactoryBot.define do
   factory :author do
-    first_name { 'MyString' }
-    last_name { 'MyString' }
-    bio { 'MyText' }
-    age { 1 }
-    publisher { 'MyString' }
   end
 end
